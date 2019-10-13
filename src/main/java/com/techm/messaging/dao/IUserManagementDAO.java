ada28b84629a30bdbe2612ab3e3fec014f04aee4
vikramsah <47307292+vikramsah@users.noreply.github.com>
2019-10-12 22:22:54 -0400

    changes

--- src/main/java/com/techm/messaging/dao/IUserManagementDAO.java
+++ src/main/java/com/techm/messaging/dao/IUserManagementDAO.java
@@ -3,7 +3,7 @@ package com.techm.messaging.dao;
 import com.techm.message.common.model.User;
 
 public interface IUserManagementDAO {
-	
+	//register
 	public User registerUser(User registerUser);
 	
 	public User folloUser(String userIdFollowing, String userIdToBeFollowed);
